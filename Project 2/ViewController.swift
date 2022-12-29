10387e336488927327d4b606df1ba659a266f475
forloop <yendheakshayd@gmail.com>
2022-12-29 12:51:56 +0530

    Initial Commit

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Akshay Yendhe on 29/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
